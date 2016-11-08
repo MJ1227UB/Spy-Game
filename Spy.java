1a525ff56b0a7d19f0a0f10b8d126fd8fdb972e7
MJ1227UB <mathias_j93@hotmail.com>
2016-11-08 09:26:44 +0100

    Spy
    Spy image updated to JPG, and image folder removed.

--- Spy.java
+++ Spy.java
@@ -12,6 +12,10 @@ public class Spy extends Mover
      * Act - do whatever the Spy wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Spy()
+    {
+        setImage("SpyIdle");
+    }
     public void act() 
     {
         // Add your action code here.
